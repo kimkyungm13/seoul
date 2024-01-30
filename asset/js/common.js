fe1f4abdaeb939b691ba5a2db353df5761a5de62
kimkyungm13 <kyung_m13@naver.com>
2024-01-30 22:27:24 +0900

    연관사이트 js

---
+++ asset/js/common.js
@@ -0,0 +1,67 @@
+$(function () {
+    const swiper = new Swiper('.mySwiper', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".paging",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
+        },
+    });
+    $('.btn-pause').click(function () {
+        $('.btn-pause').css('display', 'none');
+        $('.btn-play').css('display', 'block');
+        swiper.autoplay.stop();
+    });
+    $('.btn-play').click(function () {
+        $('.btn-play').css('display', 'none');
+        $('.btn-pause').css('display', 'block');
+        swiper.autoplay.start();
+    });
+});
+$(function () {
+    var popSwiper = new Swiper(".popSwiper", {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        slidesPerView: 3,
+        spaceBetween: 43,
+        pagination: {
+            el: ".pagination",
+            type: "fraction",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".button-next",
+            prevEl: ".button-prev",
+        },
+    });
+    $('.button-pause').click(function () {
+        $('.button-pause').css('display', 'none');
+        $('.button-play').css('display', 'block');
+        popSwiper.autoplay.stop();
+    });
+    $('.button-play').click(function () {
+        $('.button-play').css('display', 'none');
+        $('.button-pause').css('display', 'block');
+        popSwiper.autoplay.start();
+    });
+})
+
+// 연관사이트
+$(function () {
+    $('.btn-01').click(function () {
+        $('.btn-01').toggleClass('on');
+        if ($('.btn-01').hasClass('on')) {
+            $('.site_wrap').css('display', 'block');
+        } else {
+            $('.site_wrap').css('display', 'none')
+        };
+    })
+})
