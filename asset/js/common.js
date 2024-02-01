78dca95ed4cc823bad2c9d802ab68419e285a83e
kimkyungm13 <kyung_m13@naver.com>
2024-02-01 23:43:55 +0900

    0201

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,4 +1,18 @@
 $(function () {
+    $('.slide-tit a').click(function (e) { //a 태그 클릭시
+        e.preventDefault();
+        $(this).addClass('on');
+        $('.slide-tit a').not($(this)).removeClass('on');
+
+        tabName = $(this).data('slide'); //#tab1,tab2...
+        $(tabName).addClass('on').siblings().removeClass('on')
+        const swiper = new Swiper('.mySwiper');
+        swiper.update();
+        const swiper2 = new Swiper('.mySwiper2');
+        swiper2.update();
+    })
+})
+$(function () {
     const swiper = new Swiper('.mySwiper', {
         autoplay: {
             delay: 2000,
@@ -14,15 +28,42 @@ $(function () {
             prevEl: ".btn-prev",
         },
     });
-    $('.btn-pause').click(function () {
-        $('.btn-pause').css('display', 'none');
-        $('.btn-play').css('display', 'block');
+    $('#slide1 .btn-pause').click(function () {
+        $('#slide1 .btn-pause').css('display', 'none');
+        $('#slide1 .btn-play').css('display', 'block');
         swiper.autoplay.stop();
     });
-    $('.btn-play').click(function () {
-        $('.btn-play').css('display', 'none');
-        $('.btn-pause').css('display', 'block');
+    $('#slide1 .btn-play').click(function () {
+        $('#slide1 .btn-play').css('display', 'none');
+        $('#slide1 .btn-pause').css('display', 'block');
         swiper.autoplay.start();
+    });
+});
+$(function () {
+    const swiper2 = new Swiper('.mySwiper2', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        pagination: {
+            el: ".paging",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
+        },
+    });
+    $('#slide2 .btn-pause').click(function () {
+        $('#slide2 .btn-pause').css('display', 'none');
+        $('#slide2 .btn-play').css('display', 'block');
+        swiper2.autoplay.stop();
+    });
+    $('#slide2 .btn-play').click(function () {
+        $('#slide2 .btn-play').css('display', 'none');
+        $('#slide2 .btn-pause').css('display', 'block');
+        swiper2.autoplay.start();
     });
 });
 $(function () {
@@ -57,26 +98,32 @@ $(function () {
 })
 // 연관사이트
 $(function () {
+    //버튼 클릭시
     $('.site-group button').click(function () {
+        //버튼에 .on 토글하고 형제태그(site_wrap) 에도 .on 토글
         $(this).toggleClass('on').siblings().toggleClass('on');
         // 다른 버튼에 있는 on 클래스를 제거합니다.
         $('.site-group button').not($(this)).removeClass("on");
         // 다른 .site_wrap 요소에 있는 on 클래스를 제거합니다.
-        $(".site_wrap").not($(this).next(".site_wrap")).removeClass("on");
+        $(".site_wrap").not($(this).siblings(".site_wrap")).removeClass("on");
     })
 })
+
+
+
+// 상단으로 버튼
 $(function () {
-    $('.slide-tit a').click(function (e) {
-        e.preventDefault();
-        if ($('.slide-tit li').hasClass('on')) {
-            // $(this).removeClass('on').parents('li',).removeClass('on');
+    $('.btn-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        // return false; ??
+    })
+    $(window).scroll(function () {
+        current = $(this).scrollTop();
+        target = $('.header-top').offset().top;
+        if (current > target) {
+            $('.btn-top').addClass('show');
         } else {
-            $('.slide-tit li').addClass('on');
-
-        } $('.slide-tit li').not($(this)).removeClass('on');
-        // $(this).removeClass('on').parents('li',).removeClass('on');
-        tabName = $(this).data('slide'); //#tab1,tab2...
-        $(tabName).addClass('on').siblings().removeClass('on')
-
+            $('.btn-top').removeClass('show');
+        }
     })
 })
